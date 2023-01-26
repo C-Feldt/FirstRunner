086c44b08392da48ee98f12583cb1e01d21c12ad
C-Feldt <cfeldt1@lsu.edu>
2023-01-26 17:15:14 -0600

    Added Jump/Fall; Animations, Smooth movement
    Added animations for when the player is jumping/rising and falling, as well as added an input buffer which "remembers" users jump inputs for 0.2 seconds to allow pre-firing whilst in the air, leading to instantaneous jumps.

--- Assets/Hero/HeroCharacterController.cs
+++ Assets/Hero/HeroCharacterController.cs
@@ -7,13 +7,19 @@ public class HeroCharacterController : MonoBehaviour
     [SerializeField] private LayerMask groundLayers;
     [SerializeField] private float runSpeed = 8f;
     [SerializeField] private float jumpHeight = 2f;
+    [SerializeField] private Transform[] groundChecks;
+    [SerializeField] private Transform[] wallChecks;
 
     private CharacterController characterController;
     private float gravity = -50f;
     private Vector3 velocity;
     private bool isGrounded;
+    private bool isBlocked;
     private float horizontalInput;
     private Animator animator;
+    private bool jumpPressed;
+    private float jumpTimer;
+    private float jumpGracePeriod = 0.2f;
 
 
     void Start()
@@ -30,8 +36,16 @@ public class HeroCharacterController : MonoBehaviour
         // Face Forward
         transform.forward = new Vector3(horizontalInput, 0, Mathf.Abs(horizontalInput) - 1);
 
-        // Sets isGrounded
-        isGrounded = Physics.CheckSphere(transform.position, 0.1f, groundLayers, QueryTriggerInteraction.Ignore);
+        // GroundCheck: checks for whether the player is on the ground
+        isGrounded = false;
+        foreach (var groundCheck in groundChecks)
+        {
+            if(Physics.CheckSphere(groundCheck.position, 0.1f, groundLayers, QueryTriggerInteraction.Ignore))
+            {
+                isGrounded = true;
+                break;
+            }
+        }
         
         // Reset or Add gravity
         if(isGrounded && velocity.y < 0)
@@ -43,20 +57,47 @@ public class HeroCharacterController : MonoBehaviour
             velocity.y += gravity * Time.deltaTime;
         }
 
-        characterController.Move(new Vector3(horizontalInput * runSpeed, 0, 0) * Time.deltaTime);
+        // Wallcheck: checks for whether the player has run into a wall
+        isBlocked = false;
+        foreach (var wallCheck in wallChecks)
+        {
+            if(Physics.CheckSphere(wallCheck.position, 0.1f, groundLayers, QueryTriggerInteraction.Ignore))
+            {
+                isBlocked = true;
+                break;
+            }
+        }
 
-        if(isGrounded && Input.GetButtonDown("Jump"))
+        // Moves character forward if not blocked
+        if(!isBlocked)
+        {
+        characterController.Move(new Vector3(horizontalInput * runSpeed, 0, 0) * Time.deltaTime);    
+        }
+
+        // Jump
+        jumpPressed = Input.GetButtonDown("Jump");
+
+        if(jumpPressed)
+        {
+            jumpTimer = Time.time;
+        }
+
+        if(isGrounded && (jumpPressed || (jumpTimer > 0 && Time.time < jumpTimer + jumpGracePeriod)))
         {
             velocity.y += Mathf.Sqrt(jumpHeight * -2 * gravity);
+            jumpTimer = -1;
         }
 
         // Vertical Velocity
         characterController.Move(velocity * Time.deltaTime);
 
-        // Run Animation
+        // Run Animator Speed
         animator.SetFloat("Speed", horizontalInput);
 
-        // Set IsGrounded
+        // Set Animator IsGrounded
         animator.SetBool("IsGrounded", isGrounded);
+
+        // Set Animator VerticalSpeed (for Blend Tree)
+        animator.SetFloat("VerticalSpeed", velocity.y);
     }
 }
